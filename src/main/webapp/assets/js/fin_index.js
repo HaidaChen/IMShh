c4e10e456afc3dc85b78f43b7180b342fbe3494e
HaidaChen <mimi135@21cn.com>
2018-01-04 20:50:50 +0800

    for fin main page not done

---
+++ src/main/webapp/assets/js/fin_index.js
@@ -0,0 +1,44 @@
+$(function() {
+	var Accordion = function(el, multiple) {
+		this.el = el || {};
+		this.multiple = multiple || false;
+
+		// Variables privadas
+		var links = this.el.find('.link');
+		var items = $('ul.submenu').children('li');
+		// Evento
+		links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown);
+		items.on('click', {el: this.el, multiple: this.multiple}, this.active);
+	}
+
+	Accordion.prototype.dropdown = function(e) {
+		var $el = e.data.el;
+			$this = $(this),
+			$next = $this.next();
+
+		$next.slideToggle();
+		$this.parent().toggleClass('open');
+
+		if (!e.data.multiple) {
+			$el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+		};
+	}	
+	
+	Accordion.prototype.active = function(e){
+		var $el = e.data.el;
+			$this = $(this),
+			$next = $this.next();
+
+		$next.slideToggle();
+		$this.parent().toggleClass('active');
+
+		if (!e.data.multiple) {
+			$el.find('.submenu').not($next).removeClass('active');
+		};
+	}
+
+	var accordion = new Accordion($('#accordion'), false);
+	
+	
+	
+});
