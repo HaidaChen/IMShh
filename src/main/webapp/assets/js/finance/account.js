77ff48174ac324a97513502ceaaa92497246b06c
HaidaChen <12986680@qq.com>
2018-03-28 07:55:11 +0800

    do

--- src/main/webapp/assets/js/finance/account.js
+++ src/main/webapp/assets/js/finance/account.js
@@ -6,34 +6,22 @@ $(function(){
 	$("#div_add_pub").click(function(){
 		$("#div_public").append(createNewCard("div_public"));
 	});
-	
-	selectModel();
+
 });
+
+var bankLogos = [{}];
 
 var createNewCard = function(){
 	var newCard = $("<div class='col-md-3'>");
 	var panel = $("<div class='panel panel-default'>");
 	var panelHead = $("<div class='panel-heading'></div>");
 	var headRow = $("<div class='row'></div>");
-	var bankLogo = $("<div class='col-xs-2'></div>");
-	var defaultLogo = $("<i class='fa fa-image fa-2x'></i>");
-	var logoMenuStr = "<div class='model-select-box'><ul><li data-option='gongshang'>工商银行<li>";
-	logoMenuStr += "<li data-option='nongye'>农业银行<li>";
-	logoMenuStr += "<li data-option='jianshe'>建设银行<li>";
-	logoMenuStr += "<li data-option='zhongguo'>中国银行<li>";
-	logoMenuStr += "<li data-option='jiaotong'>交通银行<li>";
-	logoMenuStr += "<li data-option='zhaoshang'>招商银行<li>";
-	logoMenuStr += "<li data-option='pingan'>平安银行<li>";
-	logoMenuStr += "<li data-option='minsheng'>民生银行<li>";
-	logoMenuStr += "<li data-option='zhongxin'>中信银行<li>";
-	logoMenuStr += "<li data-option='xingye'>兴业银行<li>";	
-	logoMenuStr += "<li data-option='pufa'>浦发银行<li>";
-	logoMenuStr += "<li data-option='guangda'>光大银行<li>";
-	logoMenuStr += "<li data-option='hengfeng'>恒丰银行<li>";
-	logoMenuStr += "<li data-option='xinyongshe'>湖南农村信用社<li>";
-	logoMenuStr += "</ul></div>";
-	var logoMenu = $(logoMenuStr);
-	var cardInfo = $("<div class='col-xs-10'></div>"); 
+	var bankLogoBox = $("<div class='col-xs-2'>");
+	var bankLogo = $("<div class='dropdown'></div>");
+	var defaultLogo = $("<span class='dropdown-toggle' id='banklogoMenu' data-toggle='dropdown' aria-haspopup='true' aria-expanded='true'><i class='fa fa-image fa-2x'></i></span>");
+	
+	var logoMenu = $("<ul class='dropdown-menu' style='height:160px;overflow:scroll' aria-labelledby='banklogoMenu'>");
+	var cardInfo = $("<div class='col-xs-10'>"); 
 	var cardBank = $("<div><input type='text' name='bank' class='form-control' placeholder='请输入开户行' required='required'></div>");
 	var cardBrachBank = $("<div><input type='text' name='brachBank' class='form-control' placeholder='请输入支行'></div>");
 	var cardNo = $("<div><input type='text' name='cardNo' class='form-control' placeholder='请输入卡号' required='required'></div>");
@@ -45,21 +33,35 @@ var createNewCard = function(){
 	panel.append(panelHead);
 	panel.append(panelFooter);
 	panelHead.append(headRow);
-	headRow.append(bankLogo);
+	headRow.append(bankLogoBox);
 	headRow.append(cardInfo);
 	cardInfo.append(cardBank);
 	cardInfo.append(cardBrachBank);
 	cardInfo.append(cardNo);
 	cardInfo.append(cardUser);
+	bankLogoBox.append(bankLogo);
 	bankLogo.append(defaultLogo);
 	bankLogo.append(logoMenu);
 	panelFooter.append(operate);	
 	
+	appendLogoOption(logoMenu);
+	
 	return newCard;
 };
 
-var addLogo = function(obj){
-	
+var choseLogo = function(obj){
+	obj.parent().prev().html("<img alt='' src='" + obj.find("img").attr("src") + "'>");
+}
+
+var appendLogoOption = function(obj){
+	var oul = $(obj);
+	$.getJSON("../../../assets/json/bankLogo.json", function (data){
+		$.each(data, function(index, value){
+			var oli = $("<li><a><img alt='' src='../../../assets/images/bank/" + value.icon + ".ico'>" + value.label + "</a></li>");
+			oli.click(function(){choseLogo(oli)});
+			oul.append(oli);
+		});
+	});
 }
 
 var saveCard = function(obj){
@@ -101,56 +103,151 @@ var removeNewCard = function(obj){
 	$(obj).parents("div[class=col-md-3]").remove();
 }
 
-var selectModel = function(){
-    var $box = $('div.model-select-box');
-    var $option = $('ul.model-select-option', $box);
-    var $txt = $('div.model-select-text', $box);
-    var speed = 10;
-    /*
-     * 单击某个下拉列表时，显示当前下拉列表的下拉列表框
-     * 并隐藏页面中其他下拉列表
-     */
-    $txt.click(function(e) {
-        $option.not($(this).siblings('ul.model-select-option')).slideUp(speed, function(){
-            int($(this));
-        });
-        $(this).siblings('ul.model-select-option').slideToggle(speed, function(){
-            int($(this));
-        });
-        return false;
-    });
-    //点击选择，关闭其他下拉
-    /*
-     * 为每个下拉列表框中的选项设置默认选中标识 data-selected
-     * 点击下拉列表框中的选项时，将选项的 data-option 属性的属性值赋给下拉列表的 data-value 属性，并改变默认选中标识 data-selected
-     * 为选项添加 mouseover 事件
-     */
-    $option.find('li').each(function(index, element) {
-        if($(this).hasClass('seleced')){
-            $(this).addClass('data-selected');
-        }
-    })
-    .mousedown(function(){
-        $(this).parent().siblings('div.model-select-text').text($(this).text())
-            .attr('data-value', $(this).attr('data-option'));
-        
-        $option.slideUp(speed, function(){
-            int($(this));
-        });
-        $(this).addClass('seleced data-selected').siblings('li').removeClass('seleced data-selected');
-        return false;
-    })
-    .mouseover(function(){
-        $(this).addClass('seleced').siblings('li').removeClass('seleced');
-    });
-    //点击文档，隐藏所有下拉
-    $(document).click(function(e) {
-        $option.slideUp(speed, function(){
-            int($(this));
-        });
-    });
-    //初始化默认选择
-    function int(obj){
-        obj.find('li.data-selected').addClass('seleced').siblings('li').removeClass('seleced');
-    }
-}
+var CreditCard = function(){
+	var cardData = {};
+	
+	var root = $("<div class='col-md-3'>");
+	var panel = $("<div class='panel panel-default'>");
+	var panelHead = $("<div class='panel-heading'>");
+	var panelFooter = $("<div class='panel-footer'>");
+	
+	var cardInfoArea = $("<div class='row'>");
+	var logoArea = $("<div class='col-xs-2'>");
+	var bankInfoArea = $("<div class='col-xs-9 col-xs-offset-1'>");
+	
+	var oOpt_edit = $("<span class='pull-right'><i class='glyphicon glyphicon-edit'></i></span>");
+	var oOpt_detail = $("<span class='pull-right'><i class='glyphicon glyphicon-eye-open'></i></span>");
+	var oOpt_save = $("<span class='pull-right'><i class='glyphicon glyphicon-floppy-saved'></i></span>");
+	var oOpt_remove = $("<span class='pull-right'><i class='glyphicon glyphicon-remove-sign'></i></span>");
+	var oOpt_cancel = $("<span class='pull-right'><i class='glyphicon glyphicon-share'></i></span>");
+	
+	this.createCard = function(container, cardInfo){
+		init(container, cardInfo);		
+		
+		if (cardData.cardNo){
+			var cardNoMask = cardData.cardNo.substr(0, 4) + " **** " + cardData.cardNo.substr(cardNo.length - 4);
+			
+			var oCardLogo = $("<img alt='找不到Logo' src='../../../assets/images/bank/" + cardData.bankLogo + ".ico'>");
+			var oBankName = $("<div class='lager'>" + cardData.bankName + "</div>");
+			var oCardNo = $("<div>" + carNoMask + "</div>");
+			
+			
+			
+			logoArea.append(oCardLogo);
+			bankInfoArea.append(oBankName);
+			bankInfoArea.append(oCardNo);
+		}
+		
+		
+	}	
+	
+	var init = function(container, cardInfo){
+		container.append(root);
+		root.append(panel);
+		panel.append(panelHead);
+		panel.append(panelFooter);
+		panelHead.append(cardInfoArea);
+		cardInfoArea.append(logoArea);
+		cardInfoArea.append(bankInfoArea);	
+		
+		if (cardInfo){
+			cardData = cardInfo;			
+		}
+		
+		
+		oOpt_edit.click(function(){
+			editCard();
+		});
+		
+		oOpt_detail.click(function(){
+			
+		});
+		
+		oOpt_save.click(function(){
+			
+		});
+		
+		oOpt_remove.click(function(){
+			
+		});
+		
+		oOpt_cancel.click(function(){
+			
+		});
+	}
+	
+	var viewCard = function(){
+		var cardNoMask = cardData.cardNo.substr(0, 4) + " **** " + cardData.cardNo.substr(cardNo.length - 4);
+		
+		var oCardLogo = $("<img alt='找不到Logo' src='../../../assets/images/bank/" + cardData.bankLogo + ".ico'>");
+		var oBankName = $("<div class='lager'>" + cardData.bank + "</div>");
+		var oCardNo = $("<div>" + carNoMask + "</div>");
+				
+		clearCard();
+		
+		logoArea.append(oCardLogo);
+		bankInfoArea.append(oBankName);
+		bankInfoArea.append(oCardNo);
+		
+		panelFooter.append(oOpt_edit);
+		panelFooter.append(oOpt_detail);
+	}
+	
+	var editCard = function(){
+		var bankLogo = $("<div class='dropdown'></div>");
+		var defaultLogo = $("<span class='dropdown-toggle' id='banklogoMenu' data-toggle='dropdown' aria-haspopup='true' aria-expanded='true'><i class='fa fa-image fa-2x'></i></span>");
+		
+		var logoMenu = $("<ul class='dropdown-menu' style='height:160px;overflow:scroll' aria-labelledby='banklogoMenu'>");
+		
+		var cardNoMask = cardData.cardNo.substr(0, 4) + " **** " + cardData.cardNo.substr(cardNo.length - 4);
+		
+		var oLogoMenu = $("<div class='dropdown'>");
+		var oLogoMenuBox = $("<span class='dropdown-toggle' id='banklogoMenu' data-toggle='dropdown' aria-haspopup='true' aria-expanded='true'>");
+		var oLogo =  $("<img alt='找不到Logo' src='../../../assets/images/bank/" + cardData.bankLogo + ".ico'>");
+		
+		var oCardBank = $("<div><input type='text' name='bank' class='form-control' placeholder='请输入开户行' value='" + cardData.bank + "' required='required'></div>");
+		var oCardBrachBank = $("<div><input type='text' name='brachBank' value='" + cardData.brachBank + "' class='form-control' placeholder='请输入支行'></div>");
+		var oCardNo = $("<div><input type='text' name='cardNo' value='" + cardData.cardNo + "' class='form-control' placeholder='请输入卡号' required='required'></div>");
+		var oCardUser = $("<div><input type='text' name='cardUser' value='" + cardData.cardUser + "' class='form-control' placeholder='请输入用户名' required='required'></div>");
+				
+		clearCard();
+		
+		oLogoMenu.append(oLogoMenuBox);
+		oLogoMenuBox.append(oLogo);		
+		loadLogoOptions(oLogo);
+		
+		logoArea.append(oLogoMenu);
+		bankInfoArea.attr("class", "col-xs-10");
+		bankInfoArea.append(oCardBank);
+		bankInfoArea.append(oCardBrachBank);
+		bankInfoArea.append(oCardNo);
+		bankInfoArea.append(oCardUser);
+		
+		panelFooter.append(oOpt_save);
+		panelFooter.append(oOpt_cancel);
+		if (cardData.No)
+			panelFooter.append(oOpt_remove);
+		
+		
+	}
+	
+	var loadLogoOptions = function(obj){
+		var oul = $(obj);
+		$.getJSON("../../../assets/json/bankLogo.json", function (data){
+			$.each(data, function(index, value){
+				var oli = $("<li><a><img alt='' src='../../../assets/images/bank/" + value.icon + ".ico'>" + value.label + "</a></li>");
+				oli.click(function(){
+					oul.prev().html("<img alt='' src='" + oli.attr("src") + "'>");
+				});
+				oul.append(oli);
+			});
+		});
+	}
+	
+	var clearCard = function(){
+		logoArea.html("");
+		bankInfoArea.html("");
+		panelFooter.html("");
+		bankInfoArea.attr("class", "col-xs-9 col-xs-offset-1");
+	}
+};
